d951b2acbd60eafb6a2dd64835e8c7279638d9ea
Anuja Patil <anu415410@gmail.com>
2024-12-21 13:44:18 +0530

    Creating social media app

---
+++ src/main/java/com/anuja/socialize/SocializeAppApplication.java
@@ -0,0 +1,13 @@
+package com.anuja.socialize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocializeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocializeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anuja/socialize/SocializeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuja.socialize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocializeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
